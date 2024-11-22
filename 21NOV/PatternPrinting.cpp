7f1876cc6855252c6672ad316d49364ab493bd89
harshalthakre123 <harshalthakre0403@gmail.com>
2024-11-22 18:41:43 +0530

    new codes

---
+++ 21NOV/PatternPrinting.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // Pyramid 
+    for(int r=1; r<=5; r++)
+    {
+        for(int c=1; c<=9; c++)
+        {
+            if(c>=6-r && c<=4+r)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+       cout<<"\n";
+    }
+ 
+
+    // inverted Pyramid 
+    for(int r=2; r<=5; r++)
+    {
+        for(int c=1; c<=9; c++)
+        {
+            if(c>=r && c<=10-r)
+                cout<<"*";
+            else
+                cout<<" ";
+        }
+       cout<<"\n";
+    }
+    cout<<"\n";
+}
